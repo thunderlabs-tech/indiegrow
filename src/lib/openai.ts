c67eaf2f068dd4b07147c8f9099e12df10aab03d
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-11 08:20:22 +0200

    add openai vision analysis function

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,9 +1,57 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import { readFileSync } from "fs";
+
+// load the environment variables from .env file
 dotenv.config();
 
-export function openai() {
-  return new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+const prompt = `
+Act as a product marketing expert.
+You will begiven the contents of an App Store page for an app and an image with the screenshots.
+Analyze the app-store presense and answer the following questions:
+- What is the app about?
+- What are the key features?
+- What are the benefits?
+- Who is the target group?
+`;
+
+export async function analyzetWithLLM(
+  content: string,
+  screenshotPath: string
+): Promise<string | null> {
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  // encode the image at screenshotPath as base64
+  const imgData = readFileSync(screenshotPath).toString("base64");
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-turbo",
+    messages: [
+      {
+        role: "system",
+        content: [
+          {
+            type: "text",
+            text: prompt,
+          },
+        ],
+      },
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: `Appstore description: ${content}`,
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: `data:image/jpeg;base64,${imgData}`,
+            },
+          },
+        ],
+      },
+    ],
   });
+  const result = response.choices[0];
+  return result.message.content;
 }
