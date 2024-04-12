81238b4bd1145665c1dbc123bc2e5087ddea7545
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-12 09:55:27 +0200

    switchback to chromium-min for heroku

--- src/lib/appstore.ts
+++ src/lib/appstore.ts
@@ -1,27 +1,25 @@
 
-import puppeteer from "puppeteer";
-
-async function getBrowser() {
-  return puppeteer.launch({
-    headless: true,
-  });
-}
-
-
-// import chromium  from '@sparticuz/chromium-min';
-// import puppeteer from "puppeteer-core";
+// import puppeteer from "puppeteer";
 // async function getBrowser() {
-//     return puppeteer.launch({
-//       args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-//       defaultViewport: chromium.defaultViewport,
-//       executablePath: await chromium.executablePath(
-//         `https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar`
-//       ),
-//       headless: chromium.headless,
-//       ignoreHTTPSErrors: true,
-//     });
-
+//   return puppeteer.launch({
+//     headless: true,
+//   });
 // }
+
+
+import chromium  from '@sparticuz/chromium-min';
+import puppeteer from "puppeteer-core";
+async function getBrowser() {
+    return puppeteer.launch({
+      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(
+        `https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar`
+      ),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+}
 
 
 export type ExtractionResult = {
