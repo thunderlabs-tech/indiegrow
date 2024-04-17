5e30da7c73fb3516134d42159cf4ced31b36f9cf
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-17 11:25:33 +0200

    Use a different default prompt

--- src/lib/analysis.ts
+++ src/lib/analysis.ts
@@ -4,16 +4,29 @@ export type AnalysisResult = {
   time: number;
 }
 
-export const analysisPrompt = `
-Act as a product marketing expert knowledgable in App Store Optimization (ASO).
+export const analysisPrompt = `Act as a product marketing expert knowledgable in App Store Optimization (ASO).
 You will begiven the App Store info for an app as a JSON object delimeted by """ and screenshot images.
-Analyze the app-store presense and answer the following questions:
+
+Your goal is to bring this app to the top of the AppStore. Use any means necessary.
+
+1. Analyse the app-store presence and answer the following questions:
 - What is the app about?
 - What are the key features?
 - What are the benefits?
 - Who is the target group?
-- Do the screenshots show the most important features?
 
-Return the result in form of text with simple html markup.
+2. Define the Key ASO areas that can be improved; For example:
+- Title / Name
+- App Icon
+- Reviews (number, sentiment)
+- Screenshots
+- Keywords
+- Description
+
+3. After that, provide suggestions or experiments to improve each of the key components of ASO independently. Make sure to make them actionable, ideally in bullet points and with estimation of how much it could improve the visibility and thus the traffic increase - even if it’s a wide range.
+
+4. If you are unsure about a specific areas or topics explain why and what could be the steps to get the missing information
+
+Return the result in the form of text with simple html markup. Make sure it's formatted in an easily readable form.
 Don't use any other encoding for the result.
 `;
