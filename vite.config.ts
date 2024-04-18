d19330dc3742b1f93426d8c1e1143578167130f5
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-18 11:32:07 +0200

    kill unnecessary vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-// import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// import { OPENAI_API_KEY } from '$env/static/private';
 
 // load the environment variables from .env file
 import dotenv from 'dotenv';
