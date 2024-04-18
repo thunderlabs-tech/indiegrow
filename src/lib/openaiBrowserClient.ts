c419a0ed08042c7c80eb1ca9645f5fd3ba1fa350
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-18 12:13:23 +0200

    make openapi key public for now

--- src/lib/openaiBrowserClient.ts
+++ src/lib/openaiBrowserClient.ts
@@ -1,9 +1,11 @@
+import { PUBLIC_OPENAI_API_KEY } from '$env/static/public';
 import OpenAI from 'openai';
 
 export function openAiBrowserClient(url: string): OpenAI {
 	return new OpenAI({
-		baseURL: `${url}/llm/v1`, // use our proxy
-		apiKey: 'dummy!', // overwritten by the proxy
+		// baseURL: `${url}/llm/v1`, // use our proxy
+		// apiKey: 'dummy!', // overwritten by the proxy
+		apiKey: PUBLIC_OPENAI_API_KEY,
 		dangerouslyAllowBrowser: true
 	});
 }
