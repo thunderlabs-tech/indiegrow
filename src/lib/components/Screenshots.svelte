196449852429c12c150317c1c90f877896b7fe21
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-19 12:47:23 +0200

    kill screenshot hover style

--- src/lib/components/Screenshots.svelte
+++ src/lib/components/Screenshots.svelte
@@ -5,12 +5,7 @@
 <div class="grid grid-cols-[auto_1fr_auto] items-center gap-0">
 	<div class="flex snap-x snap-mandatory gap-2 overflow-x-auto scroll-smooth pb-2">
 		{#each screenshotUrls as url}
-			<img
-				class="rounded-container-token hover:brightness-125"
-				alt="screenshot"
-				src={url}
-				loading="lazy"
-			/>
+			<img class="rounded-container-token" alt="screenshot" src={url} loading="lazy" />
 		{/each}
 	</div>
 </div>
