1a3e6661163201dda6ed204da5c62a6b3d4b9170
Eugen Martin <eugeniusmartinus@gmail.com>
2024-05-08 12:43:13 +0200

    add projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -14,14 +14,38 @@
 			projects = data;
 		}
 	});
+
+	async function deleteProject(id: string) {
+		const db = $page.data.supabase as SupabaseClient<Database>;
+		const { error } = await db.from('projects').delete().match({ id });
+		if (error) {
+			console.log(error);
+		} else {
+			projects = projects.filter((project) => project.id !== id);
+		}
+	}
 </script>
 
-<ul>
-	{#each projects as project}
-		<li>
-			<a href="/projects/{project.id}">
-				{project.name}
-			</a>
-		</li>
-	{/each}
-</ul>
+<section class="card text-token w-full space-y-4 p-4">
+	<p class="font-bold">Projects</p>
+
+	<dl class="list-dl">
+		{#each projects as project}
+			<div>
+				<span class="badge bg-primary-500">{project.name[0]}</span>
+				<a href="/projects/{project.id}">
+					<span class="flex-auto">
+						<dt class="font-bold">{project.name}</dt>
+						<dd>{project.description}</dd>
+					</span>
+				</a>
+				<span>
+					<a class="variant-outline-primary btn btn-sm" href="/projects/{project.id}">Select</a>
+					<a class="variant-outline-warning btn btn-sm">Edit</a>
+					<a class="variant-outline-error btn btn-sm" on:click={deleteProject(project.id)}>Delete</a
+					>
+				</span>
+			</div>
+		{/each}
+	</dl>
+</section>
