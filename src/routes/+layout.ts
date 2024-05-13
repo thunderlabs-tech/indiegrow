1a993c2966d8c3684ff8e70f1520ad267e1fd4b4
Eugen Martin <eugeniusmartinus@gmail.com>
2024-05-13 13:28:15 +0200

    add vercel speedinsights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,6 +8,10 @@ import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 inject({ mode: dev ? 'development' : 'production' });
 
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
+
 export const load: LayoutLoad = async ({ fetch, data, depends, params }) => {
 	depends('supabase:auth');
 	console.log('params:', params);
