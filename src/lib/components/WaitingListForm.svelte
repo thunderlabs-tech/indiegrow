3085d8f0df86a28f04b9c92457110b9285398b19
Eugen Martin <eugeniusmartinus@gmail.com>
2024-06-11 14:31:22 +0200

    improve waiting list a bit

--- src/lib/components/WaitingListForm.svelte
+++ src/lib/components/WaitingListForm.svelte
@@ -8,6 +8,13 @@
 		const supabase = $page.data.supabase;
 		const uid = $page.data.user.id;
 
+		// validate the email:
+		const emailIsValid = email.includes('@');
+		if (!emailIsValid) {
+			console.log('email invalid');
+			return;
+		}
+
 		const entry = { email, user_id: uid };
 		console.log('Inserting: ', entry);
 		const { error } = await supabase.from('waiting_list').insert(entry);
@@ -18,14 +25,13 @@
 		}
 		return true;
 	}
-
-	const supabase = $page.data.supabase;
 </script>
 
-<div class="card">
+<div class="space-y-4 p-2">
 	{#if submitted}
 		<p class="text-green-500">You are now on the waiting list!</p>
 	{:else}
+		<h2 class="h2 text-lg font-medium">Join the Waiting List</h2>
 		<form class="space-y-4" on:submit={addToWaitingList}>
 			<input
 				bind:value={email}
@@ -33,9 +39,10 @@
 				type="email"
 				name="email"
 				id="email"
-				placeholder="Email"
+				placeholder="Your email"
 			/>
-			<button class="variant-filled-primary btn" type="submit">Join the Waiting List</button>
+			<p class="text-sm text-gray-500">We'll send you an email when we are ready for more users.</p>
+			<button class="variant-filled-primary btn" type="submit">Apply for waiting list</button>
 		</form>
 	{/if}
 </div>
