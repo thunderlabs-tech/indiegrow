6d933e8e35804c7e29845cd5c632ae10c177d45c
Eugen Martin <eugeniusmartinus@gmail.com>
2024-08-06 11:42:03 +0200

    add first version of posts display

---
+++ src/routes/blog/+page.server.ts
@@ -0,0 +1,63 @@
+import { error } from '@sveltejs/kit';
+import contentfulFetch from '$lib/contentful-fetch';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
+// const query = `
+// {
+// 	blogPage{
+//     items{
+//       name,
+//       jobTitle
+//       startDate
+//       photo {
+//         url(transform: {
+//           format: AVIF
+//         })
+//         description
+//       }
+//     }
+//   }
+// }
+// `
+
+const query = `
+{
+	blogPostCollection{
+    items{
+        title
+        content {
+            json
+        }
+    }
+  }
+}
+`;
+export async function load() {
+	const response = await contentfulFetch(query);
+
+	console.log(response);
+	if (!response.ok) {
+		throw error(response.status, {
+			message: response.statusText
+		});
+	}
+
+	const { data } = await response.json();
+
+	const { items } = data.blogPostCollection;
+	console.log(items[0].content.json);
+
+	return {
+		posts: items.map((e) => {
+			// const options = { month: 'long', year: 'numeric' };
+			// const date = new Date(e.startDate);
+			// const formattedStartDate = new Intl.DateTimeFormat('en-US', options).format(date);
+
+			return {
+				...e,
+				content: documentToHtmlString(e.content.json)
+				// startDate: formattedStartDate
+			};
+		})
+	};
+}
