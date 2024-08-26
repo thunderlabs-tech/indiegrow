3c7b1c79cb9650c0c1d6ee8b3040451c51610910
Eugen Martin <eugeniusmartinus@gmail.com>
2024-08-26 12:12:52 +0200

    fix footer

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -4,63 +4,58 @@
 </script>
 
 <div
-	class="page-footer border-t border-surface-500/10 bg-surface-50 text-xs dark:bg-surface-100-800-token md:text-base"
+	class="page-footer border-surface-500/10 bg-surface-50 text-xs dark:bg-surface-100-800-token md:text-base"
 >
-	<div class="mx-auto w-full max-w-5xl space-y-10 p-4 py-12">
+	<div class="mx-auto w-full max-w-5xl space-y-10 p-4 py-8">
 		<hr class="opacity-20" />
 		<section
-			class="flex flex-col items-center justify-between space-y-5 md:flex-row md:items-start md:space-y-0"
+			class="flex flex-col justify-between space-y-6 md:flex-row md:items-start md:space-y-0"
 		>
 			<div
 				class="grid w-2/5 grid-cols-1 place-content-center place-items-center gap-2 md:place-items-start"
 			>
 				<img src="/images/logo-2x.png" alt="IndieGrow" class="h-7" />
 			</div>
-			<div
-				class="hidden grid-cols-3 gap-8 md:grid md:place-items-start"
-				data-svelte-h="svelte-euzn36"
-			>
-				<div class="space-y-6">
-					<h6 class="h6">Content</h6>
-					<ul class="space-y-3">
-						<li><a class="anchor" href="/content/news">News</a></li>
-						<li><a class="anchor" href="/content/blog">Blog</a></li>
-						<li>
-							<a class="anchor" href="/content/aso-best-practices">ASO best practices</a>
-						</li>
-					</ul>
-				</div>
-				<div class="space-y-6">
-					<h6 class="h6">Features</h6>
-					<ul class="space-y-3">
-						<li><a class="anchor" href="/#competitor-analysis">Competitor Analysis</a></li>
-						<li><a class="anchor" href="/#community-pulse">Community Pulse</a></li>
-						<li>
-							<a class="anchor" href="/#appstore-content">AppStore Content Optimization</a>
-						</li>
-					</ul>
-				</div>
-				<div class="space-y-6">
-					<h6 class="h6">Team</h6>
-					<ul class="space-y-3">
-						<li>
-							<a
-								class="anchor"
-								href="https://www.linkedin.com/in/ionutciobotaru/"
-								target="_blank"
-								rel="noreferrer">Ionut Ciobotaru</a
-							>
-						</li>
-						<li>
-							<a
-								class="anchor"
-								href="https://www.linkedin.com/in/eugenmartin/"
-								target="_blank"
-								rel="noreferrer">Eugen Martin</a
-							>
-						</li>
-					</ul>
-				</div>
+			<div class="space-y-2 md:space-y-4">
+				<h6 class="h6">Content</h6>
+				<ul class="space-y-3">
+					<li><a class="anchor" href="/content/news">News</a></li>
+					<li><a class="anchor" href="/content/blog">Blog</a></li>
+					<li>
+						<a class="anchor" href="/content/aso-best-practices">ASO best practices</a>
+					</li>
+				</ul>
+			</div>
+			<div class="space-y-2 md:space-y-4">
+				<h6 class="h6">Features</h6>
+				<ul class="space-y-3">
+					<li><a class="anchor" href="/#competitor-analysis">Competitor Analysis</a></li>
+					<li><a class="anchor" href="/#community-pulse">Community Pulse</a></li>
+					<li>
+						<a class="anchor" href="/#appstore-content">AppStore Content Optimization</a>
+					</li>
+				</ul>
+			</div>
+			<div class="space-y-2 md:space-y-4">
+				<h6 class="h6">Team</h6>
+				<ul class="space-y-3">
+					<li>
+						<a
+							class="anchor"
+							href="https://www.linkedin.com/in/ionutciobotaru/"
+							target="_blank"
+							rel="noreferrer">Ionut Ciobotaru</a
+						>
+					</li>
+					<li>
+						<a
+							class="anchor"
+							href="https://www.linkedin.com/in/eugenmartin/"
+							target="_blank"
+							rel="noreferrer">Eugen Martin</a
+						>
+					</li>
+				</ul>
 			</div>
 		</section>
 		<hr class="opacity-20" />
