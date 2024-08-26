79b153d9eb042fe5869fddd0309f6efcf412f656
Eugen Martin <eugeniusmartinus@gmail.com>
2024-08-26 11:00:00 +0200

    fix header on mobile

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -3,44 +3,53 @@
 	import { AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<AppBar>
+<AppBar gap="none">
 	<svelte:fragment slot="lead">
-		<a href="/">
-			<img class="h-7" src="/images/logo-2x.png" alt="IndieGrow" />
+		<a href="/" class="w-32 lg:w-auto">
+			<img class="h-7 w-auto sm:h-9" src="/images/logo-2x.png" alt="IndieGrow" />
 		</a>
 	</svelte:fragment>
 	<svelte:fragment slot="default">
-		<div class="flex items-center justify-center gap-4">
+		<div class="invisible flex items-center justify-center gap-4 sm:visible">
 			<a href="/#features" class="header-link">Features</a>
 			<a href="/content" class="header-link">Content</a>
 		</div>
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
-		{#if $page.data.projects?.length > 0}
-			<a class="header-btn variant-filled-secondary btn btn-md" href="/projects">Projects</a>
-		{/if}
-		<a class="header-btn variant-filled-secondary btn btn-md" href="/#signup">Sign up</a>
+		<span class="invisible h-0 sm:visible sm:h-auto">
+			{#if $page.data.projects?.length > 0}
+				<a class="header-btn variant-filled-secondary btn btn-md" href="/projects">Projects</a>
+			{/if}
+			<a class="header-btn variant-filled-secondary btn btn-md" href="/#signup">Sign up</a>
+		</span>
 	</svelte:fragment>
 </AppBar>
 
 <style lang="postcss">
 	:global(.app-bar) {
-		width: 1200px;
-		/* height: 71px; */
-		flex-shrink: 0;
+		@apply w-full;
+		padding: 15px;
+
+		@media screen(xl) {
+			width: 1200px;
+			padding-left: 20px;
+			padding-right: 20px;
+			padding-top: 18px;
+			padding-bottom: 18px;
+		}
+
 		border-radius: 10px;
 		border: 1px solid rgba(246, 248, 237, 0.6);
 		background: rgba(246, 248, 237, 0.25);
 		backdrop-filter: blur(5px);
-		padding-left: 20px;
-		padding-right: 20px;
-		padding-top: 18px;
-		padding-bottom: 18px;
 	}
 
-	:global(#page-header) {
-		padding-top: 20px;
-		padding-bottom: 20px;
+	:global(header) {
+		padding: 15px;
+		@media screen(xl) {
+			padding-top: 20px;
+			padding-bottom: 20px;
+		}
 		/* this is what centers your element in the fixed wrapper*/
 		display: flex;
 		flex-flow: column nowrap;
